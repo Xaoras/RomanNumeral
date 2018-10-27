2940a8cf96c9917f849625a327810ef919ae02fd
Filip Nadolski <filip.nadolski@gmail.com>
2018-10-27 10:39:41 +0200

    Dodaj pliki projektów.

---
+++ UnitTestRomanNum/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RomanNumeral;
+
+namespace UnitTestRomanNum {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+            int res = Roman.fromRoman("I");
+            Assert.AreEqual(1, res);
+        }
+    }
+}
